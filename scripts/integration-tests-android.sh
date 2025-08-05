281a2f0c3e0b6221f4341021997ae2879623643b
Jan Kobersky <kober32@gmail.com>
2025-08-05 11:15:00 +0200

    Switched to ubuntu runners where possible and removed brew flutter setup

--- scripts/integration-tests-android.sh
+++ scripts/integration-tests-android.sh
@@ -7,8 +7,6 @@ set -u # stop when undefined variable is used
 # path to the script folder
 SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo "Running tests on Android device with ID: $DEVICE_ID"
-
 pushd "$SCRIPT_FOLDER/../example"
 flutter test -r expanded integration_test/integration_test.dart --ignore-timeouts
 popd
